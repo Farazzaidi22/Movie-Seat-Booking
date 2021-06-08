dbc1e4fc3a28156b28edc9ae82f52c11fe867e62
Syed Faraz Khalid <36688688+Farazzaidi22@users.noreply.github.com>
2021-06-08 06:06:46 +0500

    local storage setup completed

--- script.js
+++ script.js
@@ -5,20 +5,37 @@ const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
 let ticketPrice = +movieSelect.value //for converting from string to int
 
-console.log(ticketPrice)
+// console.log(ticketPrice)
 
 //update total and count
 function updateSelectedCount(){
     const selectedSeats =  document.querySelectorAll('.row .seat.Selected')
+    
+    const seatsIndex = [...selectedSeats].map(function(seat){
+        return [...seats].indexOf(seat)
+    })
+    
+    //local storage
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
+
     const selectedSeatsCount = selectedSeats.length
 
     count.innerText = selectedSeatsCount
     total.innerText = selectedSeatsCount * ticketPrice
 }
 
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+
+}
+
 //movie select event
 movieSelect.addEventListener('change', e=>{
     ticketPrice = +e.target.value
+    setMovieData(e.target.selectedIndex, e.target.value)
     updateSelectedCount()
 })
 
